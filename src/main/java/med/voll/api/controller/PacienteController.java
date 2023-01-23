00cc3c611392193d537f41c314856034a7574e7a
Mateus Monteiro <sensato-libentes.0k@icloud.com>
2023-01-23 14:44:08 -0300

    Novas classes e alterações

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,2 +1,15 @@
-package med.voll.api.controller;public class PacienteController {
+package med.voll.api.controller;
+
+import med.voll.api.paciente.DadosPaciente;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paciente")
+public class PacienteController {
+    public void cadastrar(@RequestBody DadosPaciente dados) {
+        System.out.println("Dados recebidos: " + dados);
+    }
+
 }
--- src/main/java/med/voll/api/paciente/DadosPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosPaciente.java
@@ -1,2 +1,6 @@
-package med.voll.api.paciente;public record DadosPaciente() {
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
 }
