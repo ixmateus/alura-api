d75d9ecc96529ac82cd1f6830193581950d81dae
Mateus Monteiro <sensato-libentes.0k@icloud.com>
2023-01-24 14:45:33 -0300

    Novas alterações e Migrations

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,12 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("paciente")
 public class PacienteController {
-    public void cadastrar(@RequestBody DadosPaciente dados) {
-        System.out.println("Dados recebidos: " + dados);
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 
 }
