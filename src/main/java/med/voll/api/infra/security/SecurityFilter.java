ece7ea20f4d17bee40b0e63b435c3a28aa97b49c
Mateus Monteiro <sensato-libentes.0k@icloud.com>
2023-03-06 17:45:29 -0300

    Recuperar Token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -12,7 +12,17 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request, response); //Necessário para que o filter seja executado após a requisição.
     }
 
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null ||!authorizationHeader.startsWith("Bearer ")) {
+            throw new RuntimeException("Token não encontrado");
+        }
+        return authorizationHeader.substring(7);
+    }
 }
